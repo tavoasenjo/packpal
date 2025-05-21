809e62325db5a40286fb650ba8f2ad1ded847f78
tavoasenjo <tavoasenjo@gmail.com>
2025-05-21 14:55:11 -0400

    Conditional return in case there are no items in the list

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -2,6 +2,12 @@ import Item from "./Item";
 
 const ListItems = ({ items, removeItem, updateItem }) => {
   // return 'add a list of items' text if there is nothing to show
+  if (items.length === 0)
+    return (
+      <h3 className='text-center text-2xl sm:text-4xl font-body w-full px-5 mt-7 sm:mt-14'>
+        Start Adding Elements To Your List! 🤩
+      </h3>
+    );
   return (
     <ul className='mt-8 sm:mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center '>
       {items.map((item) => (
